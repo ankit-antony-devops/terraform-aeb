f0082bdedcac64b44c700da1f6ed6a6ce55da547
ankit-antony-devops <116104718+ankit-antony-devops@users.noreply.github.com>
2023-08-24 18:06:48 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,23 @@ resource "aws_elastic_beanstalk_application" "docker-app" {
 }
 
 resource "aws_elastic_beanstalk_environment" "docker-env" {
-  name                = "sprintboardapp-env"
+  name                = "sprintboardapp-blue-env"
+  application         = aws_elastic_beanstalk_application.docker-app.name
+  solution_stack_name = "64bit Amazon Linux 2 v3.6.0 running Docker"
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name = "IamInstanceProfile"
+    value = "sprintboard-eb-deploy"
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name = "InstanceType"
+    value = "t2.micro"
+  }
+}
+
+resource "aws_elastic_beanstalk_environment" "docker-env" {
+  name                = "sprintboardapp-green-env"
   application         = aws_elastic_beanstalk_application.docker-app.name
   solution_stack_name = "64bit Amazon Linux 2 v3.6.0 running Docker"
   setting {
